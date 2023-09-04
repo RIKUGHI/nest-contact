4775dc6a9ebbf0fe57a36c93516795ad1bdb2d1b
RikuGhi <rifkhairkhairur@gmail.com>
2023-09-04 11:16:54 +0700

    change orm

---
+++ contact-backend/prisma/migrations/20230904035610_/migration.sql
@@ -0,0 +1,32 @@
+-- CreateTable
+CREATE TABLE `User` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(191) NOT NULL,
+    `username` VARCHAR(191) NOT NULL,
+    `password` VARCHAR(191) NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `Contact` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `first_name` VARCHAR(191) NOT NULL,
+    `last_name` VARCHAR(191) NOT NULL,
+    `email` VARCHAR(191) NOT NULL,
+    `phone` VARCHAR(191) NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `Address` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `street` VARCHAR(191) NOT NULL,
+    `city` VARCHAR(191) NOT NULL,
+    `province` VARCHAR(191) NOT NULL,
+    `country` VARCHAR(191) NOT NULL,
+    `postal_code` VARCHAR(191) NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
--- contact-backend/src/users/entities/user.entity.ts
+++ contact-backend/src/users/entities/user.entity.ts
@@ -1,16 +1,9 @@
-import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
-
-@Entity()
 export class User {
-  @PrimaryGeneratedColumn()
   id: number;
 
-  @Column()
   name: string;
 
-  @Column()
   username: string;
 
-  @Column()
   password: string;
 }
